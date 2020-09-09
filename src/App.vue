f63d673e1dd737d923655a3449fc28466074e700
Андрей Бурмистров <andrejburmistrov@MacBook-Pro-Andrej.local>
2020-09-09 23:49:11 +0700

    9

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld texts="Тестовая кнопка покупки" title="Корзина" /> -->
+    <ul :class="'products'">
+      <li
+        @mouseenter="product.hover = true"
+        @mouseleave="product.hover = false"
+        :class="product.hover ? 'active' : ''"
+        v-for="product in products"
+        v-bind:key="product.id"
+      >
+        <img v-if="product.img" :src="product.img" />
+        <div class="card-content">
+        <a :href="product.link"><h2>{{ product.post_title }}</h2></a>
+        <p>{{ product.excerpt }}</p>
+        <p class="pack">{{ product.pack }}</p>
+        <p class="price">{{ product.price }}<span class="currency"> p.</span></p>
+        </div>
+        <p><button :class="product.incart ? 'btn-incart' : 'btn-order'" @click="product.incart = !product.incart; saveCart(product.id, this.cart)">{{product.incart ? 'В Заказе' : 'Заказать'}}</button></p>
+      </li>
+    </ul>
+    <ContactForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld";
+import ContactForm from "./components/ContactForm";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    //HelloWorld,
+    ContactForm
+  },
+  data() {
+    return {
+      products: [],
+      cart: []
+    };
+  },
+  methods: {
+    saveCart: (id, cart) => {
+      if (cart.indexOf(id) === -1) {cart.push(id)};
+      localStorage.cart = JSON.stringify(cart);
+    },
+    fetchdata: function() {
+      //fetch(`https://jsonplaceholder.typicode.com/users`)
+      fetch("https://thai-open.ru/json-api/")
+        .then(response => response.json())
+        .then(json => {
+          this.products = json;
+        });
+    }
+  },
+  mounted() {
+    this.fetchdata();
+    if (localStorage.cart) {
+      this.cart = JSON.parse(localStorage.cart);
+    }
   }
 };
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Exo 2", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.products {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 15px -15px;
+  padding: 0;
+}
+.products li {
+  margin: 15px;
+  border-radius: 10px;
+  border: 1px solid #d4d4d4;
+  overflow: hidden;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.products li.active {
+  box-shadow: 0 0 10px #d4d4d4;
+}
+.products img {
+  width: 300px;
+  height: 192px;
+  object-fit: cover;
+}
+.products .pack {
+  color:#009FE0;
+}
+.products .card-content {
+  padding: 0 10px 10px 10px;
+}
+.products .card-content p {
+  margin-bottom: 0;
+}
+.products .price {
+  font-size: 20px;
+  font-weight: bold;
+}
+.products .currency {
+  position: absolute;
+  font-weight: normal;
+  margin-left: 0.25rem;
+}
+.products button {
+  text-transform: uppercase;
+  outline: none;
+}
+.products a {
+  color: inherit;
+  text-decoration: none;
+}
+.products a:hover {
+  color:#009FE0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,73 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h3>{{ title }}</h3>
+    <p>{{ texts }}</p>
+    <button type="button" @click="LoadData">{{ buy.title }}</button>
+    <div class="buttons-wrap">
+      <button type="button" class="minus" @click="minusOne"></button>
+      {{ buy.count }} ед.
+      <button type="button" class="plus" @click="plusOne"></button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  props: ["title", "texts"],
+  data() {
+    return {
+      buy: {
+        title: "В КОРЗИНУ",
+        count: 1
+      }
+    };
+  },
+  mounted() {
+    if (localStorage.buy) {
+      this.buy = JSON.parse(localStorage.buy);
+    }
+  },
+  methods: {
+    plusOne: function() {
+      this.buy.count++;
+    },
+    minusOne: function() {
+      if (this.buy.count > 0) {
+        this.buy.count--;
+      }
+    },
+    LoadData: function() {
+      if (this.buy.title === "ОФОРМИТЬ") {
+        this.buy.title = "ОТПРАВКА...";
+        fetch(
+          "https://api.telegram.org/bot1391995122:AAE7G9DRDozp-VduV0IdjkmM1jERFOV1nKU/sendMessage?chat_id=-431711252&parse_mode=html&text=заказ на " +
+            this.buy.count +
+            " ед."
+        )
+          .then(response => response.json())
+          .then(json => {
+            console.log("ответ: " + JSON.stringify(json));
+            setTimeout(() => (this.buy.title = "ГОТОВО!"), 1000);
+            setTimeout(() => (this.buy.title = "В КОРЗИНУ"), 3000);
+          });
+      }
+      switch (this.buy.title) {
+        case "В КОРЗИНУ":
+          this.buy.title = "ОФОРМИТЬ";
+          break;
+        default:
+          break;
+      }
+    }
+  },
+  watch: {
+    buy: {
+      handler: function(buy, newBuy) {
+        localStorage.buy = JSON.stringify(newBuy);
+      },
+      deep: true
+    }
   }
 };
 </script>
